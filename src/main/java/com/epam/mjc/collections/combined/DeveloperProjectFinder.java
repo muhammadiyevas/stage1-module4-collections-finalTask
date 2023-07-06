0b1b7647396eb70fcc53fe18fcd5b89920989b7e
MuhammadiyevaSevinch <muhammadiyeva133@gmail.com>
2023-07-06 09:54:57 +0500

    collections ->finalTask

--- src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
+++ src/main/java/com/epam/mjc/collections/combined/DeveloperProjectFinder.java
@@ -1,10 +1,37 @@
 package com.epam.mjc.collections.combined;
 
-import java.util.List;
-import java.util.Map;
-import java.util.Set;
+import java.util.*;
 
 public class DeveloperProjectFinder {
     public List<String> findDeveloperProject(Map<String, Set<String>> projects, String developer) {
+        List<String> result = new ArrayList<>();
+
+        for (String project : projects.keySet()) {
+            Set<String> developers = projects.get(project);
+            if (developers.contains(developer)) {
+                result.add(project);
+            }
+        }
+
+        Collections.sort(result, new Comparator<String>() {
+            @Override
+            public int compare(String project1, String project2) {
+                int lengthComparison = Integer.compare(project2.length(), project1.length());
+                if (lengthComparison != 0) {
+                    return lengthComparison;
+                } else {
+                    return project2.compareTo(project1);
+                }
+            }
+        });
+
+        return result;
     }
 }
+
+
+
+
+
+
+
--- src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
+++ src/main/java/com/epam/mjc/collections/combined/MapFromKeysCreator.java
@@ -1,9 +1,24 @@
 package com.epam.mjc.collections.combined;
 
+import java.util.HashMap;
+import java.util.HashSet;
 import java.util.Map;
 import java.util.Set;
 
 public class MapFromKeysCreator {
     public Map<Integer, Set<String>> createMap(Map<String, Integer> sourceMap) {
+        Map<Integer, Set<String>> sortList = new HashMap<>();
+
+        for (Map.Entry<String, Integer> list : sourceMap.entrySet()) {
+            String key = list.getKey();
+            int length = key.length();
+
+            Set<String> keySet = sortList.getOrDefault(length, new HashSet<>());
+
+            keySet.add(key);
+            sortList.put(length, keySet);
+        }
+
+        return sortList;
     }
 }
